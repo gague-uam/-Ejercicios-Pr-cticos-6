12901c5facf93b5c989fb0f95f7111af0b56fd95
gague-uam <ggpaiz@uamv.edu.ni>
2024-05-02 16:39:14 -0600

    Ejercicio 2 Imprimir numeros impares del 1 al 100

---
+++ Ejercicios2/ImprimirImpares.cpp
@@ -0,0 +1,21 @@
+/*Desarrollar un programa que imprima los números impares en orden
+descendente que hay entre 1 y 100.*/
+#include <iostream>
+using namespace std;
+
+void numImpar(int num);
+
+int main() {
+    int num = 100;
+    cout << "Numeros impares del 1 al " << num << " en orden descendente:" << endl;
+    numImpar(num);
+    return 0;
+}
+
+ void numImpar(int num){
+    for (int i = num; i >= 1; i--) {
+        if (i % 2 != 0) {
+            cout << i << " ";
+        }
+    }
+}
